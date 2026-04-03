5092c12c5b4db11bd4def1faf42a7cd7efb638a9
Hunchojoe19 <enubiakjoseph@gmail.com>
2026-04-03 20:57:29 +0100

    chore: update copyright notice to Doc Photography in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -76,7 +76,7 @@ const Footer = () => {
             <a href="#" className="hover:text-white transition"><Linkedin className="w-4 h-4" /></a>
           </div>
 
-          <p>© 2024 Damien Braun Photography. All rights reserved.</p>
+          <p>© 2024 Doc Photography. All rights reserved.</p>
         </div>
       </div>
     </footer>
