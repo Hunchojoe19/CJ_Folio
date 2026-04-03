72cefafa9826c266c722f5d28694b7276ead3cd5
Hunchojoe19 <enubiakjoseph@gmail.com>
2026-04-03 22:55:45 +0100

    refactor: remove unused lucide-react social icons and add react-icons dependency

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -59,7 +59,7 @@ const About = () => {
                 </div>
               </div>
             </div>
-            <FaInstagram />
+
             {/* Socials & Actions */}
             <div className="flex items-center mt-auto">
               {/* Social icons */}
