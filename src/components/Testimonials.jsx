b87e16423b6f88418151761332c6deee2e8fa0b3
Hunchojoe19 <enubiakjoseph@gmail.com>
2026-04-03 22:56:03 +0100

    refactor: remove unused social media icons from Testimonials component imports

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ArrowLeft, ArrowRight, ArrowDownRight, Facebook, Twitter, Linkedin, Star } from 'lucide-react';
+import { ArrowLeft, ArrowRight, ArrowDownRight, Star } from 'lucide-react';
 
 const testimonials = [
   {
